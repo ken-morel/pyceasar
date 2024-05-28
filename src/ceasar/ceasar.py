7f710bd787a2745205ecfa8c3144859fbb75be0c
ken-morel <engonken8@gmail.com>
2024-05-28 15:29:01 +0100

    moved ceasar to ceasar/ and created ceasar.py

--- src/ceasar/ceasar.py
+++ src/ceasar/ceasar.py
@@ -1,7 +1,3 @@
-from pathlib import Path
-from argparse import ArgumentParser
-
-
 class Dictionary:
     min_len = 3
     file = None
@@ -87,65 +83,3 @@ def unceasar(txt, dictionnary):
     else:
         text = best[0][0]
     return text
-
-
-def ExistingPath(url):
-    path = Path(url)
-    assert path.exists(), f"path '{path!r}' does not exist"
-    return path
-
-
-parser = ArgumentParser(
-    prog='ceasar',
-    description='A little program to cipher and uncipher ceasar cipher',
-)
-
-parser.add_argument(
-    'infile',
-    type=ExistingPath,
-    help='The input file to read text from',
-)
-parser.add_argument(
-    'outfile',
-    type=Path,
-    help='The file to write output to',
-)
-
-parser.add_argument(
-    '-ceasar --c',
-    dest='ceasar',
-    type=int,
-    default=3
-)
-
-parser.add_argument(
-    '-unceasar --u',
-    dest='unceasar',
-    action='store_true',
-)
-
-parser.add_argument(
-    '--dictionary -d',
-    type=Dictionary,
-    dest='dictionary',
-    help=(
-        'The path to a file containing the words to be counted'
-        ' seperated by newlines'
-    ),
-    default=Dictionary(
-        Path(__file__).resolve().parent / 'dictionary/en.txt',
-    ),
-)
-
-args = parser.parse_args()
-
-
-if args.unceasar:
-    txt = args.infile.read_text()
-    print(args.dictionary)
-    un = unceasar(txt, args.dictionary)
-    args.outfile.write_text(un)
-else:
-    txt = args.infile.read_text()
-    ce = ceasar(txt, args.ceasar)
-    args.outfile.write_text(ce)
